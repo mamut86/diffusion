1b2cdc3cf399ee1ea0cb8a047b0446772384b5c7
Oliver Schär <Oliver Schr>
2017-03-01 22:32:31 +0100

    added Gompertz example

--- example.R
+++ example.R
@@ -1,16 +1,19 @@
+
+
+# Norton Bass example -----------------------------------------------------
 library(systemfit)
 source("bass.R")
 source("nortonbass.R")
-load("dram.RData")
+load("data/dram.Rdram")
 
-fitNB1 <- nortonBass(data, startval.met = "2ST", startval = NULL, flexpq = F, gstart = NULL)
-fitNB2 <- nortonBass(data, startval.met = "2ST", startval = NULL, flexpq = T, gstart = NULL)
+fitNB1 <- nortonBass(dram, startval.met = "2ST", startval = NULL, flexpq = F, gstart = NULL)
+fitNB2 <- nortonBass(dram, startval.met = "2ST", startval = NULL, flexpq = T, gstart = NULL)
 
 # Create some plots
-plot(data[,1],type = "l", ylim=c(0,30000))
-lines(data[,2],col ="blue")
-lines(data[,3],col ="green")
-lines(data[,4],col ="pink")
+plot(dram[,1],type = "l", ylim=c(0,30000))
+lines(dram[,2],col ="blue")
+lines(dram[,3],col ="green")
+lines(dram[,4],col ="pink")
 
 lines(fitNB1$fitted[[1]]$predicted, col = "black", lty = 2)
 lines(fitNB1$fitted[[2]]$predicted, col = "blue", lty = 2)
@@ -32,3 +35,14 @@ fitNB2$fitted[[1]]$rmse
 fitNB2$fitted[[2]]$rmse
 fitNB2$fitted[[3]]$rmse
 fitNB2$fitted[[4]]$rmse
+
+
+# Gompertz model example --------------------------------------------------
+
+source("gompertz.R")
+load("data/chicken.RData")
+
+Gompertz(x = chicken$weight)
+
+load("data/cars.RData")
+Gompertz(cars$raw)
