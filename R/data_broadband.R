9fdcd0f49f72130cc293dfc93f7c5528a8e99b63
Schaer <schaer@lancs.local>
2017-03-20 19:05:31 +0000

    added data sample

---
+++ R/data_broadband.R
@@ -0,0 +1,14 @@
+#' Broadcast subscribers
+#' 
+#' A dataset containing the broadcast subscribers to UK market 4Q 2011 to 2Q 2016.
+#' 
+#' @format A data frame with 51 observations and 4 variables
+#' \describe{
+#' \item{time}{Quarters}
+#' \item{DSL}{Adoption of DSL subscribers}
+#' \item{CableModem}{Adoption of CableModem users}
+#' \item{FTTPb}{Adoption of FTTPb sales}
+#'   }
+#'   
+#' @references Telecoms Market Matrix
+"broadband"
